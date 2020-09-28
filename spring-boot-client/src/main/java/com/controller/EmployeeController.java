6d81ffa701bd092cfd9920c783e333cd26157c19
tsrahul1996 <45625496+tsrahul1996@users.noreply.github.com>
2020-09-28 19:28:50 +0530

    Add files via upload
    commit 1

---
+++ spring-boot-client/src/main/java/com/controller/EmployeeController.java
@@ -0,0 +1,86 @@
+package com.controller;
+
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.model.Employee;
+
+
+@Controller
+public class EmployeeController {
+	@RequestMapping(value = "/getEmployees", method = RequestMethod.GET)
+	public ModelAndView getEmployeeInfo() {
+		return new ModelAndView("getEmployees");
+	}
+
+	@RequestMapping(value = "/showEmployees", method = RequestMethod.GET)
+	public ModelAndView showEmployees() throws JsonProcessingException, IOException {
+		ResponseEntity<String> response = null;
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		// According OAuth documentation we need to send the client id and secret key in the header for authentication
+		String credentials = "RMXGiKgTtMDBc2D2oCr8FHIP7:80JRWQJqqrqTxTDtgqhgrD9mJ0FjfJTQXfKhEcLm1NPIUbbHQb";
+		String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.add("Authorization",  encodedCredentials);
+
+		
+		
+		HttpEntity<String> request = new HttpEntity<String>(headers);
+
+		String access_token_url = "https://api.twitter.com/oauth2/token";
+		access_token_url += "&grant_type=client_credentials";
+
+		response = restTemplate.exchange(access_token_url, HttpMethod.POST , request, String.class);
+
+		System.out.println("Access Token Response ---------" + response.getBody());
+
+		/*
+		 * curl -u "$API_KEY:$API_SECRET_KEY" \ --data 'grant_type=client_credentials' \
+		 * 'https://api.twitter.com/oauth2/token'
+		 */
+		
+		// Get the Access Token From the recieved JSON response
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode node = mapper.readTree(response.getBody());
+		String token = node.path("access_token").asText();
+		
+		System.out.println("Access Token Response token: ---------" +token);
+
+		String url = "http://localhost:8080/user/getEmployeesList";
+
+		/*
+		 * // Use the access token for authentication HttpHeaders headers1 = new
+		 * HttpHeaders(); headers1.add("Authorization", "Bearer " + token);
+		 * HttpEntity<String> entity = new HttpEntity<>(headers1);
+		 * 
+		 * ResponseEntity<Employee[]> employees = restTemplate.exchange(url,
+		 * HttpMethod.GET, entity, Employee[].class); System.out.println(employees);
+		 * Employee[] employeeArray = employees.getBody();
+		 */
+
+		ModelAndView model = new ModelAndView("showEmployees");
+		model.addObject("employees", Arrays.asList(""));
+		return model;
+	}
+}
